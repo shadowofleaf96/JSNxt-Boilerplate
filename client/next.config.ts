cf3cc1020a2cf5dd485e58974661abdbe6ec9ee5
Shadow Of Leaf <shadowofleaf96@gmail.com>
2025-05-05 00:53:32 +0100

    fix: try to use local yaml

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,4 +1,5 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
   async headers() {
@@ -25,6 +26,13 @@ const nextConfig: NextConfig = {
         hostname: "**",
       },
     ],
+  },
+  webpack(config) {
+    config.resolve.alias = {
+      ...(config.resolve.alias ?? {}),
+      "@": path.resolve(__dirname, "src"),
+    };
+    return config;
   },
   // output: "export",
   reactStrictMode: true,
