3ee761c5db97964a7ff706ace52bbd0c794e27d5
Shadow Of Leaf <shadowofleaf96@gmail.com>
2025-09-09 17:37:13 +0100

    fix: fix loading button in userForm

--- client/src/components/ui/LoadingSpinner.tsx
+++ client/src/components/ui/LoadingSpinner.tsx
@@ -1,17 +1,18 @@
 import React from 'react';
 
 interface LoadingSpinnerProps {
-  size?: number;
+  size?: number; // in pixels
   className?: string;
 }
 
 const LoadingSpinner: React.FC<LoadingSpinnerProps> = ({
-  size = 12,
+  size = 24,
   className = '',
 }) => {
   return (
     <svg
-      className={`w-${size} h-${size} m-auto text-gray-300 animate-spin ${className}`}
+      style={{ width: size, height: size }}
+      className={`m-auto animate-spin ${className}`}
       viewBox="0 0 64 64"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
@@ -22,15 +23,14 @@ const LoadingSpinner: React.FC<LoadingSpinnerProps> = ({
         strokeWidth="5"
         strokeLinecap="round"
         strokeLinejoin="round"
-      ></path>
+      />
       <path
         d="M32 3C36.5778 3 41.0906 4.08374 45.1692 6.16256C49.2477 8.24138 52.7762 11.2562 55.466 14.9605C58.1558 18.6647 59.9304 22.9531 60.6448 27.4748C61.3591 31.9965 60.9928 36.6232 59.5759 40.9762"
         stroke="currentColor"
         strokeWidth="5"
         strokeLinecap="round"
         strokeLinejoin="round"
-        className="text-gray-900"
-      ></path>
+      />
     </svg>
   );
 };
