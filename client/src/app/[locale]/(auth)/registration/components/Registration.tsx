6a649428c5c2c538db722748ca5c064cc989ce93
Shadow Of Leaf <shadowofleaf96@gmail.com>
2025-09-10 14:58:17 +0100

    fix: implement loadingSpinner fix that i forgot 2

--- client/src/app/[locale]/(auth)/registration/components/Registration.tsx
+++ client/src/app/[locale]/(auth)/registration/components/Registration.tsx
@@ -225,11 +225,20 @@ const Registration: React.FC = () => {
                 </div>
               )}
 
-              <Button type="submit" className="w-full mt-2" disabled={loading}>
-                {loading ? <LoadingSpinner size={20} /> : t('register.submit')}
-              </Button>
-
-              <p className="text-xs text-center text-muted-foreground mt-4">
+              <div>
+                <button
+                  type="submit"
+                  disabled={loading}
+                  className="flex w-full justify-center rounded-md bg-black px-4 py-3 text-sm font-semibold text-white shadow-sm hover:bg-gray-500 focus-visible:outline focus-visible:outline-offset-2 focus-visible:outline-gray-600 disabled:opacity-60"
+                >
+                  {loading ? (
+                    <LoadingSpinner size={20} />
+                  ) : (
+                    t('register.submit')
+                  )}
+                </button>
+              </div>
+              <p className="text-xs text-center text-gray-500 mt-6">
                 {t('register.recaptcha_disclaimer')}{' '}
                 <a
                   href="https://policies.google.com/privacy"
